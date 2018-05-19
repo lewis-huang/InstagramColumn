122ad1c3326939862b28141b5b65c521261b1a2e
Lewis <huangyun_122@hotmail.com>
2018-05-19 21:21:22 +0800

    Initial commit

---
+++ FetchInstagram/FetchInstagram/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FetchInstagram
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(114, 172);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(42, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(59, 12);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "ColumnUrl";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(114, 19);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(581, 82);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 123);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(89, 12);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "ColumnDetected";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(114, 123);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(581, 21);
+			this.textBox1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(777, 211);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ FetchInstagram/FetchInstagram/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+using System.Text.RegularExpressions;
+
+namespace FetchInstagram
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			MySqlConnectionStringBuilder connString = new MySqlConnectionStringBuilder();
+			connString.Server = "192.168.1.112";
+			connString.UserID = "huangyun";
+			connString.Password = "huangyun";
+			connString.Database = "pythondb";
+
+			MySqlConnection connection = new MySqlConnection(connString.ConnectionString);
+			MySqlCommand cmd = connection.CreateCommand();
+			connection.Open();
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.CommandText = "insertNewColumn";
+			cmd.Parameters.Add(new MySqlParameter("pColumnName", richTextBox1.Text));
+			cmd.Parameters.Add(new MySqlParameter("pColumnUrl", textBox1.Text));
+
+			cmd.ExecuteNonQuery();
+			connection.Close();
+
+
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+			string columnurl = richTextBox1.Text;
+			Regex filterReg = new Regex(@".com[/]([\s\S])*", RegexOptions.IgnoreCase);
+			Regex subfixReg = new Regex(@"[/]([\s\S])*[/]", RegexOptions.IgnoreCase);
+
+			if (filterReg.IsMatch(columnurl))
+			{
+				if (subfixReg.IsMatch(filterReg.Match(columnurl).Value))
+				{
+					textBox1.Text = subfixReg.Match(filterReg.Match(columnurl).Value).Value.Replace('/', '.');
+				}
+			}
+		}
+	}
+}
